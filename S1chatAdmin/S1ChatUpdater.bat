c1e0df02ffd5a1154b5aa192740b448d6a35ab82
X3nom <100533068+X3nom@users.noreply.github.com>
2023-01-30 23:25:24 +0100

    multiple directories are now one variable

--- S1chatAdmin/S1ChatUpdater.bat
+++ S1chatAdmin/S1ChatUpdater.bat
@@ -1,10 +1,11 @@
 @echo off
 :: developer tool, allows to easily update S1Chat folder for all users (users = paths leading to destinations specified in users.txt)
+set ADMIN_S1CHAT="\\server1\userA\S1Chat"
 :a
 set /p user="enter user to recieve update (enter "all" to update for all specified users in users.txt): "
 if %user%== all ( goto all )
-robocopy \\server1\userA\S1Chat \\server1\%user%\S1Chat
+robocopy %ADMIN_S1CHAT% \\server1\%user%\S1Chat
 goto a
 :all
-for /F "tokens=*" %%A in ( \\server1\userA\S1Chat ) do robocopy \\server1\userA\S1Chat \\server1\%%A\S1Chat
+for /F "tokens=*" %%A in ( %ADMIN_S1CHAT% ) do robocopy %ADMIN_S1CHAT% \\server1\%%A\S1Chat
 goto a
