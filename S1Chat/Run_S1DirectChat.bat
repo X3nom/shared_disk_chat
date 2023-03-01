ed3678d5224f6c7a02c1968413b66227d64e9c6f
X3nom <100533068+X3nom@users.noreply.github.com>
2023-03-01 13:27:56 +0100

    Update Run_S1DirectChat.bat

--- S1Chat/Run_S1DirectChat.bat
+++ S1Chat/Run_S1DirectChat.bat
@@ -5,14 +5,17 @@ set ALL_USERS_ROOT="\\server1\"
 cd sys
 start S1inboxWindow.bat
 cd ..
-echo type /msg [username] to change destination of your messages
-set /p user="enter user to send messages to: "
+:change
+cls
+echo [0mtype /msg to change destination of your messages
+set /p user="[0menter user to send messages to: [36m"
 for /d /r %ALL_USERS_ROOT% %%a in (*) do if /i %%~nxa==%user% set user_path=%%a
 set inbox_path="%user_path%\S1chat\data\inbox.txt"
 cls
 :a
-echo type /msg [username] to change destination of your messages
-set /p msg="enter msg for %user%: [36m"
+echo [0mtype /msg to change destination of your messages
+set /p msg="enter msg for [36m%user%[0m: [33m"
+if %msg%== /msg ( goto change )
 echo [34m%username%^> [33m%msg% >> %inbox_path%
 set msg=""
 echo "[0m"
